9f85614a25e3ac2ffbc21c0ea8c38afc6bdc4bd9
abner <1060163350@qq.com>
2018-11-19 16:19:19 +0800

    update import.py

--- import.py
+++ import.py
@@ -5,18 +5,44 @@
 从 data.txt 中读入数据
 """
 
-buliding = []
+class basestation(object):
+    def __init__(self, number, xpos, ypos, height, power):
+        self.number = number
+        self.xpos = xpos
+        self.ypos = ypos
+        self.height = height
+        self.power = power
+
 basestation = []
 
-infile = open('beijingdata.txt', 'r')
+"""
+basestation = [
+                [1, 443359, 4428792, 18, 24.2],
+                [2, 444783, 4427964, 14, 24.2]
+            ]
+"""
+
+# print basestation
+
+infile = open('testdata.txt', 'r')
+
+line = infile.readline()
+
+while line:
+    print line,
 
 
-
-
-
-
-
-
-
+    line = infile.readline()
 
 infile.close()
+
+
+""" 读文件
+with open('odom.txt', 'r') as f:
+    data = f.readlines()  #txt中所有字符串读入data
+ 
+    for line in data:
+        odom = line.split()        #将单个数据分隔开存好
+        numbers_float = map(float, odom) #转化为浮点数
+        print numbers_float
+"""
