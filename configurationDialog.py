8e978ed6239c4a6a771dba72998242a539efb741
abner <1060163350@qq.com>
2018-11-21 23:13:09 +0800

    update

--- configurationDialog.py
+++ configurationDialog.py
@@ -16,33 +16,66 @@ class configurationDialog(QtGui.QWidget):
         self.initUI()
 
     def initUI(self):
+        
+        thermalNoise = QtGui.QLabel(u'   热噪声')
+        bandwidth = QtGui.QLabel(u'     带宽')
+        frequency = QtGui.QLabel(u'     频率')
+        RSRPthreshold = QtGui.QLabel(u'RSRP 门限')
 
-        title = QtGui.QLabel('Title')
-        author = QtGui.QLabel('Author')
-        review = QtGui.QLabel('Review')
+        thermalNoiseEdit = QtGui.QLineEdit()
+        bandwidthEdit = QtGui.QLineEdit()
+        frequencyEdit = QtGui.QLineEdit()
+        RSRPthresholdEdit = QtGui.QLineEdit()
 
-        titleEdit = QtGui.QLineEdit()
-        authorEdit = QtGui.QLineEdit()
-        reviewEdit = QtGui.QTextEdit()
+        thermalNoiseUnit = QtGui.QLabel('dBm/Hz\t')
+        bandwidthUnit = QtGui.QLabel('Hz\t')
+        frequencyUnit = QtGui.QLabel('MHz\t')
+        RSRPthresholdUnit = QtGui.QLabel('dBm\t')
 
-        # 创建网格布局对象，并设置部件之间的间距。
-        grid = QtGui.QGridLayout()
-        grid.setSpacing(10)
+        saveButton = QtGui.QPushButton(u'保存并退出')
+        exitButton = QtGui.QPushButton(u'仅退出')
+        exitButton.clicked.connect(self.close)
 
-        grid.addWidget(title, 1, 0)
-        grid.addWidget(titleEdit, 1, 1)
+        # 创建水平框布局对象，添加伸缩因子和两个按钮。
+        # 这个伸缩因子在窗口改变时会在其前面添加空白。
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(saveButton)
+        hbox.addWidget(exitButton)
 
-        grid.addWidget(author, 2, 0)
-        grid.addWidget(authorEdit, 2, 1)
+        h1 = QtGui.QHBoxLayout()
+        h1.addWidget(thermalNoise)
+        h1.addWidget(thermalNoiseEdit)
+        h1.addWidget(thermalNoiseUnit)
 
-        # 添加部件到网格中，并指定跨越的行。
-        grid.addWidget(review, 3, 0)
-        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+        h2 = QtGui.QHBoxLayout()
+        h2.addWidget(bandwidth)
+        h2.addWidget(bandwidthEdit)
+        h2.addWidget(bandwidthUnit)
 
-        self.setLayout(grid)
+        h3 = QtGui.QHBoxLayout()
+        h3.addWidget(frequency)
+        h3.addWidget(frequencyEdit)
+        h3.addWidget(frequencyUnit)
 
-        self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('Review')
+        h4 = QtGui.QHBoxLayout()
+        h4.addWidget(RSRPthreshold)
+        h4.addWidget(RSRPthresholdEdit)
+        h4.addWidget(RSRPthresholdUnit)
+
+        # 创建一个垂直框布局对象，并将水平框布局对象添加到垂直框布局对象中。
+        vbox = QtGui.QVBoxLayout()
+        vbox.addLayout(h1)
+        vbox.addLayout(h2)
+        vbox.addLayout(h3)
+        vbox.addLayout(h4)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        # 设置主窗口的布局。
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 350, 180)
+        self.setWindowTitle(u'新建配置')
         self.show()
 
 def main():
