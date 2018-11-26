f9887c2fec4c1c40013b161ece18413fc289ec0e
abner <1060163350@qq.com>
2018-11-26 09:43:02 +0800

    update

--- import.py
+++ import.py
@@ -5,23 +5,9 @@
 从 data.txt 中读入数据
 """
 
-"""
-import os
-with open((os.path.join('testdata.txt')), 'r') as f:
-	data = f.read()
-
-strlist = data.split('md5=')
-for item in strlist[1:]:
-	try:
-		list_link = item.split(' target=_blank')[0]
-		print list_link
-	except:
-		pass
-"""
-import matplotlib.pyplot as plt
-
+Data = []
 with open('testdata.txt', 'r') as f:
-    X, Y, H, P = zip(*[[float(s) for s in line.split()] for line in f])
-
-plt.plot(X, Y, H, P)
-plt.show()
+		for line in f:
+			data = line.split()
+			Data.append(data)
+		print Data[1][1]
